c35fa6c4b45ff7e4148d43a16f8d7c60a8e91fca
Luke Arduini <luke.arduini@me.com>
2013-01-02 22:06:03 -0500

    [fix] merge issue

--- test/test.js
+++ test/test.js
@@ -1,18 +1,13 @@
 var dumpling = require('../')
 var test = require('tap').test
-var fs = require('fs')
 
 test('convert php junk', function (t) {
-  var input = fs.createReadStream('input.txt', {encoding: 'utf8'})
+  var input = require('./input')
+  var output = require('./output')
 
-  input.on('data', function (input) {
-    var input = dumpling(input)
-    
-    var output = fs.createReadStream('output.json', {encoding: 'utf8'})
-    output.on('data', function (output) {
-      t.equal(output, JSON.stringify(input))
-      t.end()
+  input = JSON.stringify(dumpling(input))
+  output = JSON.stringify(output)
 
-    })
-  })
+  t.equal(output, input)
+  t.end()
 })
