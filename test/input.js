780269e0653d87756cd93f8505b1f1e6ae431429
Luke Arduini <luke.arduini@me.com>
2013-01-02 21:54:46 -0500

    [test] changes for testling-ci

---
+++ test/input.js
@@ -0,0 +1,8 @@
+var session = ['user|s:4:"3221";key|s:27:"d93hf9wjr9f3jw93jdj2jd9si8a";secret',
+'|s:7:"abcdefg";3055e5738562918b67f6e6407a6cfa80__id|s:32:"d93hf9wjr9f3jw93jdj',
+'2jd9si8a28cha";d93hf9wjr9f3jw93jdj2jd9si8a__name|s:4:"3381";d93hf9wjr9f3jw93jd',
+'j2jd9si8a__states|a:0:{};']
+
+module.exports = (function () {
+  return session.join('')
+})()
---
+++ test/output.js
@@ -0,0 +1,12 @@
+var output = { 
+  user: '3221',
+  key: 'd93hf9wjr9f3jw93jdj2jd9si8a',
+  secret: 'abcdefg',
+  '3055e5738562918b67f6e6407a6cfa80__id': 'd93hf9wjr9f3jw93jdj2jd9si8a28cha',
+  d93hf9wjr9f3jw93jdj2jd9si8a__name: '3381',
+  d93hf9wjr9f3jw93jdj2jd9si8a__states: '{}' 
+}
+
+module.exports = (function () {
+  return output
+})()
