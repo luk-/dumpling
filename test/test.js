3309555f44c010330e9d57890da9a969c4569188
Luke Arduini <luke.arduini@me.com>
2013-01-03 01:16:36 -0500

    [test] fix for older browsers

--- test/test.js
+++ test/test.js
@@ -1,6 +1,8 @@
 var dumpling = require('../')
 var test = require('tape')
 
+typeof JSON === 'undefined' && (JSON = require('jsonify'))
+
 test('convert php junk', function (t) {
   var input = require('./input')
   var output = require('./output')
